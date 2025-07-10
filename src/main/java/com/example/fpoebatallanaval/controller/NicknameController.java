c11f3a5f5375e86a36a32a9616d12346ec92e266
Brandon Lasprilla Aristizabal <brandon.lasprilla@correounivalle.edu.co>
2025-07-09 21:49:29 -0500

    nickname fix

--- src/main/java/com/example/fpoebatallanaval/controller/NicknameController.java
+++ src/main/java/com/example/fpoebatallanaval/controller/NicknameController.java
@@ -1,5 +1,6 @@
 package com.example.fpoebatallanaval.controller;
 
+import com.example.fpoebatallanaval.models.GameDataManager;
 import com.example.fpoebatallanaval.views.GameView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,7 +12,8 @@ import java.io.IOException;
 
 public class NicknameController {
 
-    @FXML private TextField textFieldNickname;
+    @FXML
+    private TextField textFieldNickname;
 
 
     @FXML
@@ -20,6 +22,19 @@ public class NicknameController {
         if (nickname.isBlank()) {
             nickname = "Jugador"; // Default nickname
         }
+        //updates the nickname
+        GameDataManager.saveNickname(nickname);
+
+        //declara el booleano para saber si el jugador existe o es nuevo
+        boolean yaRegistrado = GameDataManager.playerExists(nickname);
+
+        //declara por defecto el numero de barcos hundidos si el jugador es nuevo
+        if (!yaRegistrado) {
+            GameDataManager.updateBarcosHundidos(nickname, 0);
+        }
+
+        //guarda el nickname y el numero de barcos hundidos en players.txt
+        GameDataManager.savePlayerStats();
 
         // Load the game view (singleton)
         GameView gameView = GameView.getInstance();
@@ -30,5 +45,9 @@ public class NicknameController {
         // Close the current menu window
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         currentStage.close();
+
+
+
+
     }
 }
